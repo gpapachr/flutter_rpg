d24b352eb570df0ae07aef00b8219ff04c5d0bb8
Giannis Papachristou <giannis.papachristou@vodafoneinnovus.com>
2024-08-30 18:20:20 +0300

    Implement home and create screens without transition between them

---
+++ lib/screens/create/create.dart
@@ -0,0 +1,161 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rpg/models/character.dart';
+import 'package:flutter_rpg/models/vocation.dart';
+import 'package:flutter_rpg/screens/create/vocation_card.dart';
+import 'package:flutter_rpg/shared/styled_button.dart';
+import 'package:flutter_rpg/shared/styled_text.dart';
+import 'package:flutter_rpg/theme.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:uuid/uuid.dart';
+
+var uuid = const Uuid();
+
+class Create extends StatefulWidget {
+  const Create({super.key});
+
+  @override
+  State<Create> createState() => _CreateState();
+}
+
+class _CreateState extends State<Create> {
+  final _nameController = TextEditingController();
+  final _sloganController = TextEditingController();
+
+  @override
+  void dispose() {
+    // clean up the controller when the widget is disposed
+    _nameController.dispose();
+    _sloganController.dispose();
+    super.dispose();
+  }
+
+  // handling vocation selection
+  Vocation selectedVocation = Vocation.junkie;
+
+  void updateVocation(Vocation vocation) {
+    setState(() {
+      selectedVocation = vocation;
+    });
+  }
+
+  // submit handler
+  void handleSubmit() {
+    if (_nameController.text.trim().isEmpty) {
+      // show error in dialog
+      return;
+    }
+    if (_sloganController.text.trim().isEmpty) {
+      // show error in dialog
+      return;
+    }
+
+    characters.add(Character(
+      name: _nameController.text,
+      slogan: _sloganController.text,
+      vocation: selectedVocation,
+      id: uuid.v4(),
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const StyledTitle('Character Creation'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // welcome message
+              Center(
+                child: Icon(Icons.code, color: AppColors.primaryColor),
+              ),
+              const Center(
+                child: StyledHeading('Welcome, new player.'),
+              ),
+              const Center(
+                child: StyledText('Create a name & slogan for your character.'),
+              ),
+              const SizedBox(height: 30),
+
+              // input for name & slogan
+              TextField(
+                controller: _nameController,
+                cursorColor: AppColors.textColor,
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.person_2),
+                  label: StyledText('Character name'),
+                ),
+                style: GoogleFonts.kanit(
+                  textStyle: Theme.of(context).textTheme.bodyMedium,
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: _sloganController,
+                cursorColor: AppColors.textColor,
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.chat),
+                  label: StyledText('Character slogan'),
+                ),
+                style: GoogleFonts.kanit(
+                  textStyle: Theme.of(context).textTheme.bodyMedium,
+                ),
+              ),
+              const SizedBox(height: 30),
+
+              // select vocation title
+              Center(
+                child: Icon(Icons.code, color: AppColors.primaryColor),
+              ),
+              const Center(child: StyledHeading('Choose a Vocation.')),
+              const Center(
+                  child: StyledText('This determines your available skills.')),
+              const SizedBox(height: 30),
+
+              // vocation cards
+              VocationCard(
+                vocation: Vocation.junkie,
+                onTap: updateVocation,
+                selected: selectedVocation == Vocation.junkie,
+              ),
+              VocationCard(
+                vocation: Vocation.ninja,
+                onTap: updateVocation,
+                selected: selectedVocation == Vocation.ninja,
+              ),
+              VocationCard(
+                vocation: Vocation.wizard,
+                onTap: updateVocation,
+                selected: selectedVocation == Vocation.wizard,
+              ),
+              VocationCard(
+                vocation: Vocation.raider,
+                onTap: updateVocation,
+                selected: selectedVocation == Vocation.raider,
+              ),
+
+              // good luck message
+              Center(
+                child: Icon(Icons.code, color: AppColors.primaryColor),
+              ),
+              const Center(child: StyledHeading('Good Luck.')),
+              const Center(child: StyledText('And enjoy the journey...')),
+              const SizedBox(height: 30),
+
+              // submit button
+              Center(
+                child: StyledButton(
+                  onPressed: handleSubmit,
+                  child: const StyledHeading('Create Character'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
